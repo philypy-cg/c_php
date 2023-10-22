3e1e8635d6bf02df1535ae7d064e588ec902bee1
Philypy Gomes <phcg.dev@gmail.com>
2023-10-22 20:18:54 -0300

    var e const

---
+++ ex004/index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tipo primitivos em PHP</title>
+</head>
+
+<body>
+    <h1>Teste de tipos primitivos</h1>
+
+    <?php 
+        // 0x =hexadecimal 0b=binario 0=octal
+        // $num = 010;
+        // echo"O valor da variavel é $num";
+
+        // $v = "Gomes";
+        // var_dump($v);
+
+        // $num = 3e2; //3x10(2)
+        // echo "O valor é $num";
+
+        // $vet = [6, 2.5, "Jeh", 3, true];
+        // var_dump($vet); array
+
+        // class Pessoa {
+        //     private string $nome;
+        // }
+        // $p = new Pessoa;
+        // var_dump($p); obejtect
+    ?>
+
+</body>
+
+</html>
