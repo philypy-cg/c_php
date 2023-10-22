9430be2ad0a7dd83e8a433d38cc094e273a390f9
Philypy Gomes <phcg.dev@gmail.com>
2023-10-22 01:01:39 -0300

    servphpprimeiro

---
+++ ex003/index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Variaveis e Constantes</title>
+</head>
+
+<body>
+    <h1>Variaveis e Constantes</h1>
+    <?php
+        $nome="Philypy";
+        $sobrenome="Gomes";
+        $idade = 31;
+        $peso = 72.5;
+        $casado = true;
+        
+        const ESTADO = "Pernambuco - ";
+        const PAIS = "Brasil";
+        const CURSO = "TI";
+        
+        
+        echo "Muito prazer, $nome $sobrenome! Voce mora no Estado de " . ESTADO . PAIS
+    
+    
+    ?>
+
+</body>
+
+</html>
